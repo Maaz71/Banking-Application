a22fabb5ba37d49ff547c7913dab86d5ee5c8c28
Maaz71 <maazrehman789@gmail.com>
2024-11-02 01:53:03 +0530

    Created controller's request handling createUser method

---
+++ banking-app/src/main/java/com/app/banking_app/controller/AccountController.java
@@ -0,0 +1,30 @@
+package com.app.banking_app.controller;
+
+import com.app.banking_app.dto.AccountDto;
+import com.app.banking_app.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // makes class as SpringMVC Rest Controller class
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private AccountService accountService;  //no need to use @Autowired anno as this class has only 1 cnstrt so spring will automatically inject dependency
+
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //add Account Rest Api
+    @PostMapping
+    public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto) //@Requestbody converts incoming json into java object
+    {
+
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+}
--- banking-app/src/main/java/com/app/banking_app/service/impl/AccountServiceImpl.java
+++ banking-app/src/main/java/com/app/banking_app/service/impl/AccountServiceImpl.java
@@ -1,6 +1,8 @@
 package com.app.banking_app.service.impl;
 
 import com.app.banking_app.dto.AccountDto;
+import com.app.banking_app.mapper.AccountMapper;
+import com.app.banking_app.pojo.Account;
 import com.app.banking_app.repository.AccountRespository;
 import com.app.banking_app.service.AccountService;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,10 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public AccountDto createAccount(AccountDto accountDto) {
-        return null;
+
+        Account account = AccountMapper.mapToAccount(accountDto);
+        Account savedAccount =accountRespository.save(account); // save() return saved entity
+
+        return AccountMapper.mapToAccountDto(savedAccount);
     }
 }
